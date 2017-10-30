ab88cb74eeeef6966f944575a5cb57d5be35d78a
imota <imota@antevenio.com>
2017-10-30 16:10:55 +0100

    Can't get tags from outside master in "prefer-dist" composer mode.

--- src/Predis/FailsafeClient.php
+++ src/Predis/FailsafeClient.php
@@ -1,12 +1,14 @@
 <?php
 namespace Antevenio\Redis\Predis;
-
 use Predis\Connection\ConnectionException;
-
-class FailsafeClient extends \Predis\Client
+class FailsafeClient
 {
     const RETRY_TIMES = 10;
-
+    protected $client;
+    public function __construct(\Predis\Client $client)
+    {
+        $this->client = $client;
+    }
     public function __call($method, $args)
     {
         $retries = 0;
@@ -14,7 +16,7 @@ class FailsafeClient extends \Predis\Client
         do {
             $retries++;
             try {
-                $retValue = call_user_func_array(array($this, $method), $args);
+                $retValue = call_user_func_array(array($this->client, $method), $args);
                 $retryCall = false;
             } catch (ConnectionException $ex) {
                 echo $ex->getMessage() . "\n";
